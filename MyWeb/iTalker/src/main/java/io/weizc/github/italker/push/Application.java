f41d100214ce3c45d1c7d3e5dea055241d950f09
Vzc <564063517@qq.com>
2017-06-05 23:09:29 +0800

    服务端API学习

--- MyWeb/iTalker/src/main/java/io/weizc/github/italker/push/Application.java
+++ MyWeb/iTalker/src/main/java/io/weizc/github/italker/push/Application.java
@@ -1,7 +1,19 @@
 package io.weizc.github.italker.push;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import io.weizc.github.italker.push.service.AcountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
 /**
  * Created by Vzc on 2017/6/5.
  */
-public class Application {
+public class Application extends ResourceConfig{
+    public Application() {
+        //注册逻辑处理的包名
+        packages(AcountService.class.getPackage().getName());
+        //注册JSON转化器
+        register(JacksonJsonProvider.class);
+
+
+    }
 }
---
+++ MyWeb/iTalker/src/main/java/io/weizc/github/italker/push/service/AcountService.java
@@ -0,0 +1,29 @@
+package io.weizc.github.italker.push.service;
+
+import io.weizc.github.italker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+/**
+ * Created by Vzc on 2017/6/5.
+ */
+@Path("/account")
+public class AcountService {
+    @GET
+    @Path("/login")
+    public String get(){
+        return "YOU get the login.";
+    }
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post(){
+        User user = new User();
+        user.setName("二狗子");
+        user.setSex(1);
+        return user;
+    }
+}
