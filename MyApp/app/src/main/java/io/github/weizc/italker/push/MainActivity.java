0f14ac7ce06153ae85aaa7fd02f44f373afc828a
Vzc <564063517@qq.com>
2017-06-12 22:59:20 +0800

    主页面初始化，以及lang包初始化

--- MyApp/app/src/main/java/io/github/weizc/italker/push/MainActivity.java
+++ MyApp/app/src/main/java/io/github/weizc/italker/push/MainActivity.java
@@ -1,19 +1,22 @@
 package io.github.weizc.italker.push;
 
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 import io.github.weizc.itakler.common.app.Activity;
 
 public class MainActivity extends Activity implements IView {
-    @BindView(R.id.tv_main)
-    TextView tv_mian;
-    @BindView(R.id.et_main)
-    EditText editText;
-    IPresenter mPresenter;
 
+    @BindView(R.id.appbar)
+    View mLayoutAppBar;
 
     @Override
     protected int getContentLayoutId() {
@@ -21,19 +24,31 @@ public class MainActivity extends Activity implements IView {
     }
 
 
-    @OnClick(R.id.bt_main)
-    public void submit() {
-        if (mPresenter == null) mPresenter = new Presenter(this);
-        mPresenter.search();
-    }
+
 
     @Override
     public void setResultString(String result) {
-        tv_mian.setText(result);
+
+    }
+
+    @Override
+    protected void initWidget() {
+        super.initWidget();
+        Glide.with(this)
+                .load(R.mipmap.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View,GlideDrawable>(mLayoutAppBar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable glideDrawable, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(glideDrawable.getCurrent());
+                    }
+
+
+                });
     }
 
     @Override
     public String getResultString() {
-        return String.valueOf(editText.getText());
+        return null;
     }
 }
---
+++ MyApp/common/src/main/java/io/github/weizc/itakler/common/widget/PortraitView.java
@@ -0,0 +1,25 @@
+package io.github.weizc.itakler.common.widget;
+
+        import android.content.Context;
+        import android.util.AttributeSet;
+
+        import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * @author Vzc  Email:newlr@foxmail.com
+ * @version 1.0.0
+ * @Date 2017/6/12.
+ */
+public class PortraitView extends CircleImageView {
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
